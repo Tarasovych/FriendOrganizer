a3999732320048965669107fcf7887281e3bb134
Tarasovych <tarasovych.ua@gmail.com>
2017-12-10 02:14:52 +0200

    Entity added

---
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,27 @@
+namespace FriendOrganizer.DataAccess.Migrations
+{
+    using FriendOrganizer.Model;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizer.DataAccess.FriendOrganizerDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FriendOrganizer.DataAccess.FriendOrganizerDbContext context)
+        {
+            context.Friends.AddOrUpdate(
+                f => f.FirstName,
+                new Friend { FirstName = "Thomas", LastName = "Smith" },
+                new Friend { FirstName = "Test", LastName = "Tost" },
+                new Friend { FirstName = "Hot", LastName = "Rod" },
+                new Friend { FirstName = "Number", LastName = "Four" }
+                );
+        }
+    }
+}
