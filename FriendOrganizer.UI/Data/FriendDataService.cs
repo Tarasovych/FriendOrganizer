7b94022cd5afbcfe316e8fe1e896ae06a17df3db
Tarasovych <tarasovych.ua@gmail.com>
2018-01-03 19:04:04 +0200

    Data added

--- FriendOrganizer.UI/Data/FriendDataService.cs
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -1,18 +1,25 @@
-﻿using FriendOrganizer.Model;
+﻿using FriendOrganizer.DataAccess;
+using FriendOrganizer.Model;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FriendOrganizer.UI.Data
 {
     public class FriendDataService : IFriendDataService
     {
+        private Func<FriendOrganizerDbContext> _contextCreator;
+
+        public FriendDataService(Func<FriendOrganizerDbContext> contextCreator)
+        {
+            _contextCreator = contextCreator;
+        }
         public IEnumerable<Friend> GetAll()
         {
-            // TODO: db
-            yield return new Friend { FirstName = "name1", LastName = "last1" };
-            yield return new Friend { FirstName = "name2", LastName = "last2" };
-            yield return new Friend { FirstName = "name3", LastName = "last3" };
-            yield return new Friend { FirstName = "name4", LastName = "last4" };
-            yield return new Friend { FirstName = "name5", LastName = "last5" };
+            using (var ctx = _contextCreator())
+            {
+                return ctx.Friends.AsNoTracking().ToList();
+            }
         }
     }
 }
--- FriendOrganizer.UI/Startup/Bootstrapper.cs
+++ FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using FriendOrganizer.DataAccess;
 using FriendOrganizer.UI.Data;
 using FriendOrganizer.UI.ViewModel;
 
@@ -9,6 +10,8 @@ namespace FriendOrganizer.UI.Startup
         public IContainer Bootstrap()
         {
             var builder = new ContainerBuilder();
+
+            builder.RegisterType<FriendOrganizerDbContext>().AsSelf();
 
             builder.RegisterType<MainWindow>().AsSelf();
             builder.RegisterType<MainViewModel>().AsSelf();
